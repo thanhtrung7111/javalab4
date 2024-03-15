336838dfa81930eb7d2ee0f272f3f08f067bb3c8
thanhtrung <thanhtrung71119999@gmail.com>
2024-03-15 12:47:55 +0700

    commit haha

--- src/main/java/com/poly/lab3/StaffServlet.java
+++ src/main/java/com/poly/lab3/StaffServlet.java
@@ -38,7 +38,7 @@ public class StaffServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello than sádasdassfh");
+		System.out.println("Hello than sádasdassfh sdfsdfsdf");
 		request.getRequestDispatcher("/views/lab3/form-register.jsp").forward(request, response);
 	}
 
